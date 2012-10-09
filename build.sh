339ccbc0cf3bac4a5eb5f6bc453ed97a714665cc
Stefan Seyfried <seife+dev@b1-systems.com>
2012-10-09 09:57:39 +0000

    build.sh: autodetect running kernel flavour

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#
+# hack to find SUSE kernel flavour of the running kernel
+read a b V d < /proc/version
+V=${V##*-}
 
-make -C /usr/src/linux-obj/x86_64/xen M=$PWD $@
+make -C /usr/src/linux-obj/x86_64/$V M=$PWD $@
