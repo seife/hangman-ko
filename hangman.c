831202c1f74308c9657a7351b9794116ebeb86ec
Stefan Seyfried <seife+dev@b1-systems.com>
2012-10-16 05:33:26 +0000

    hangman: add more hang methods
    more hang modes added, selectable via module parameter
    the pointers to private kernel data structures need to be
    extracted from System.map for now

--- hangman.c
+++ hangman.c
@@ -1,27 +1,83 @@
 #include <linux/module.h>
 #include <linux/delay.h>
 #include <linux/kernel.h>
+#include <linux/sched.h> /* tasklist_lock */
 
-int init_module(void)
+/* for delayed triggers */
+static struct timer_list timer;
+
+static int irqsave2;
+module_param(irqsave2, int, 0);
+MODULE_PARM_DESC(irqsave2, "hang via 2 x spin_lock_irqsave in init function");
+
+static int irqsave;
+module_param(irqsave, int, 0);
+MODULE_PARM_DESC(irqsave, "hang via unbalanced spin_lock_irqsave in timer");
+
+static int tasklist;
+module_param(tasklist, int, 0);
+MODULE_PARM_DESC(tasklist, "hang via unbalanced tasklist_lock");
+
+static int proc_subdir;
+module_param(proc_subdir, int, 0);
+MODULE_PARM_DESC(proc_subdir, "hang via unbalanced proc_subdir_lock");
+
+static void timer_func(unsigned long d);
+static spinlock_t lck;
+static unsigned long flags;
+
+/* those pointer values need to be taken from System.map for now */
+rwlock_t *my_tasklist_lock = (rwlock_t *)0xffffffff81a05040;
+spinlock_t *my_proc_subdir_lock = (spinlock_t *)0xffffffff81cf7de0;
+
+static int __init hangman_init(void)
 {
-	unsigned long flags;
-	spinlock_t lck;
-	spin_lock_init(&lck);
-	spin_lock_irqsave(&lck, flags);
-	printk(KERN_INFO "*hangman*...\n");
-#if 0
-	ssleep(10);
-	spin_unlock_irqrestore(&lck, flags);
-#endif
-	spin_lock_irqsave(&lck, flags);
-	printk(KERN_INFO "     *hangs*\n");
+	printk(KERN_INFO "%s...\n", __func__);
+	if (irqsave2) {
+		printk(KERN_INFO "taking spin_lock_irqsave twice...\n");
+		spin_lock_init(&lck);
+		spin_lock_irqsave(&lck, flags);
+		spin_lock_irqsave(&lck, flags); /* instant deadlock */
+	}
+	if (irqsave) {
+		/* start timer function */
+		spin_lock_init(&lck);
+		init_timer(&timer);
+		timer.function = timer_func;
+		timer.expires = jiffies + HZ;
+		add_timer(&timer);
+	}
+	if (tasklist) {
+		printk(KERN_INFO "taking tasklist_lock \n");
+		read_lock(my_tasklist_lock);
+	}
+	if (proc_subdir) {
+		printk(KERN_INFO "taking my_proc_subdir_lock\n");
+		spin_lock(my_proc_subdir_lock);
+	}
+	printk(KERN_INFO "%s done.\n", __func__);
 
 	return 0;
 }
 
-void cleanup_module(void)
+static void __exit hangman_exit(void)
 {
+	printk(KERN_INFO "hangman unloading\n");
 	return;
 }
 
+module_init(hangman_init);
+module_exit(hangman_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Stefan Seyfried");
+MODULE_DESCRIPTION("This module hangs your kenrel in various ways");
+
+static void timer_func(unsigned long d)
+{
+	printk(KERN_INFO "hangman: timer function\n");
+	spin_lock_irqsave(&lck, flags);
+	timer.expires = jiffies + HZ;
+	add_timer(&timer);
+}
 
